81cba4673eb06963de7b8e10254f14085ea2c111
Chris Abbod <chrisabbod@Chris-MacBook-Pro.local>
2019-05-06 19:47:12 -0400

    Tiles flash and play correct sound

--- game.js
+++ game.js
@@ -1,18 +1,17 @@
 
 var buttonColors = ["red", "blue", "green", "yellow"];
-var randomChosenColor = buttonColors[nextSequence()];
-
 var gamePattern = [];
 
 function nextSequence() {
-    var randomNumber = Math.floor(Math.random() * 4);
-    return randomNumber;
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColor = buttonColors[randomNumber];
+
+  gamePattern.push(randomChosenColor);
+
+  $("#" + randomChosenColor).fadeOut(100).fadeIn(100);
+
+  var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+  audio.play();
+
+  console.log(gamePattern);
 }
-
-gamePattern.push(randomChosenColor);
-
-$("#" + randomChosenColor).fadeOut(100).fadeIn(100);
-
-console.log(randomChosenColor);
-console.log(gamePattern);
-console.log($("#" + randomChosenColor));
